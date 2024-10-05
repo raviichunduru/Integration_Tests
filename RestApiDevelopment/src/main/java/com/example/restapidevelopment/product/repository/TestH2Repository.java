4d19f9c27b56ce691fefad2b3ded72d094b94d2c
admin <ravii.chunduru@gmail.com>
2024-10-05 15:06:02 +0530

    initial commit

--- RestApiDevelopment/src/main/java/com/example/restapidevelopment/product/repository/TestH2Repository.java
+++
@@ -1,9 +0,0 @@
-package com.example.restapidevelopment.product.repository;
-
-import com.example.restapidevelopment.product.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface TestH2Repository extends JpaRepository<Product,Integer> {
-}
